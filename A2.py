df4fe6ca15f969ae6a58e7a895d96b8f9034d626
edilson gomes de lima <edskateicevida@gmail.com>
2023-07-05 00:21:30 -0300

    add list

---
+++ A2.py
@@ -0,0 +1,7 @@
+nome_masculinos = ["joão", 'bruna', 'erika', 'liza']
+
+nomes_femininos = ['Maria', 'Ana', 'livia']
+print('Nomes masculinos:', nome_masculinos)
+
+Nomes_outros = ['Alex', 'patricia', 'carlos']
+print('Outros Nomes:', Nomes_outros)
